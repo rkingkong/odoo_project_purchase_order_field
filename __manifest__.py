371bd6886206791b45d4afd74837653d32922a32
Rodolfo Kong <kongrodolfo@gmail.com>
2025-08-10 09:06:03 -0600

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Project Task Purchase Orders & Start Date',
+    'version': '16.0.1.0.0',
+    'summary': 'Adds Purchase Order links and Start Date to project tasks',
+    'category': 'Project',
+    'author': 'ARMKU LLC',
+    'depends': ['project', 'purchase'],
+    'data': [
+        'views/project_task_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
